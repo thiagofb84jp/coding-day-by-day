74e307503b58f3c33626b81ad9d2a66417970bd6
thiagofb84jp <thiagofb84jp@gmail.com>
2020-01-28 14:07:01 -0300

    Inclusão dos exercícios de Javascript do curso da Udemy

--- exercicios-js/objeto/oo.js
+++ exercicios-js/objeto/oo.js
@@ -1,7 +1,13 @@
 /**
  * Pontos Importantes!
  * 1. Este exercício deste bloco não será um código executável!
- * 2. A ideia da Orientação a Objetos é estar mais próximo do mundo real.
+ * 2. A ideia da Orientação a Objetos é estar mais próximo do mundo real;
+ * 3. Os princípios importantes são:
+ *    a) Abstração;
+ *    b) Encapsulamento;
+ *    c) Herança (prototype);
+ *    d) Polimorfismo.
+ * 4. Durante o desenvolvimento, priorize a composição em vez da Herança.
  */
 
  //Procedural
@@ -17,4 +23,4 @@
      }
  }
 
- objeto.processamento();
+ objeto.processamento(); //Foco passou a ser o objeto
