22bea9b5442e047d5bfbd082fc89ec83daba073b
thiagofb84jp <thiagofb84jp@gmail.com>
2020-03-23 00:26:16 -0300

    Inclusão de novos exercícios do curso de Java da Udemy

---
+++ curso-java/exercicios/src/oo/heranca/Jogador.java
@@ -0,0 +1,16 @@
+package oo.heranca;
+
+public class Jogador {
+	
+	int x;
+	
+	int y;
+	
+	boolean andar(String direcao) {
+		if ("norte".equalsIgnoreCase(direcao)) {
+			y++;
+		}
+		
+		return true;
+	}
+}
