351b8de5e802b9f6c7fdbea00c489a008cfe48d7
thiagofb84jp <thiagofb84jp@gmail.com>
2020-03-29 11:35:58 -0300

    Inclusão de novos exercícios da parte de Spring Boot

--- curso-spring-boot/workspace/cursomc/src/main/java/br/com/cursoudemy/cursomc/resources/CategoriaResource.java
+++ curso-spring-boot/workspace/cursomc/src/main/java/br/com/cursoudemy/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package br.com.cursoudemy.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.javafaker.Faker;
 
@@ -19,27 +22,35 @@ import javassist.tools.rmi.ObjectNotFoundException;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		Categoria obj = categoriaService.find(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		 obj = categoriaService.insert(obj);
+		 
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		 
+		 return ResponseEntity.created(uri).build();
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
 		Categoria cat1 = new Categoria(1, new Faker().company().name());
 		Categoria cat2 = new Categoria(2, new Faker().company().name());
-		
+
 		List<Categoria> listaCategoria = new ArrayList<>();
 		listaCategoria.add(cat1);
 		listaCategoria.add(cat2);
-		
+
 		return listaCategoria;
 	}
 }
