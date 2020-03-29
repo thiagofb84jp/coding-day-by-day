8b53f091330837140208e56412ce4db5d18660d0
thiagofb84jp <thiagofb84jp@gmail.com>
2020-03-29 11:16:23 -0300

    Inclusão de novos ajustes/exercícios do curso de Spring Boot

--- curso-spring-boot/workspace/cursomc/src/main/java/br/com/cursoudemy/cursomc/domain/Cliente.java
+++ curso-spring-boot/workspace/cursomc/src/main/java/br/com/cursoudemy/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.cursoudemy.cursomc.enums.TipoCliente;
 
@@ -36,7 +35,6 @@ public class Cliente implements Serializable{
 
 	private Integer tipoCliente; // 'Macete' para que o tipoCliente seja armazenado como um tipo Inteiro
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
@@ -44,7 +42,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
